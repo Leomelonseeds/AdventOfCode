ab9f723a836c1def065ca63da34f352a195cb365
Leomelonseeds <leonardfang15@gmail.com>
2022-12-21 23:22:22 -0800

    Finish day 22

---
+++ src/com/leomelonseeds/aoc/y2022/day24/Day24_1.java
@@ -0,0 +1,8 @@
+package com.leomelonseeds.aoc.y2022.day24;
+
+public class Day24_1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/com/leomelonseeds/aoc/y2022/day24/Day24_2.java
@@ -0,0 +1,8 @@
+package com.leomelonseeds.aoc.y2022.day24;
+
+public class Day24_2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
